09e761094a4beefffe17f1628fcd83915de628fc
Nicolas Araya Ventura <150105875+nicolasarayaventura@users.noreply.github.com>
2025-09-06 23:57:29 -0400

    add

--- recitation/lab01/month.cpp
+++ recitation/lab01/month.cpp
@@ -10,34 +10,47 @@ You may not use switch case or arrays even if you know these language constructs
 */
 #include <iostream>
 using namespace std;
+
+// Check if a year is a leap year
+bool LeapYear(int year) {
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
+
 int main() {
     cout << "Enter year: ";
     int year;
     cin >> year;
-   
-    cout << "Enter month:";
+
+    cout << "Enter month: ";
     int month;
     cin >> month;
 
-    string out1 = "31 days";
-    string out2 = "30 days";
-
-    // first 7 months
-    if (month <= 7) {
-        if (month % 2 == 0) {
-            cout << out2 << endl;
+    // February case
+    if (month == 2) {
+        if (LeapYear(year)) {
+            cout << "29 days" << endl;
         } else {
-            cout << out1 << endl;
-        }
-    // months 8- 12
-    } else {
-        if (month % 2 == 0) {        
-            cout << out1 << endl;
-        } else {
-            cout << out2 << endl;
+            cout << "28 days" << endl;
         }
     }
-return 0;
+    // First 7 months
+    else if (month <= 7) {
+        if (month % 2 == 0) {
+            cout << "30 days" << endl;
+        } else {
+            cout << "31 days" << endl;
+        }
+    }
+    // Months 8-12
+    else {
+        if (month % 2 == 0) {
+            cout << "31 days" << endl;
+        } else {
+            cout << "30 days" << endl;
+        }
+    }
+
+    return 0;
 }
 
 
