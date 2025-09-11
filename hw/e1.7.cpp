64c66b9cb5b7dafcf1c126b48eda426fba62f48b
Nicolas Araya Ventura <150105875+nicolasarayaventura@users.noreply.github.com>
2025-09-11 08:49:20 -0400

    add

---
+++ hw/e1.7.cpp
@@ -0,0 +1,18 @@
+/*
+Author: Nicolas Araya Ventura
+Course: CSCI-136
+Instructor: Sarah Khaldi
+Assignment: Homework e1.7
+
+Write a program that prints three items, such as the names of your three best friends
+or favorite movies, on three separate lines.
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "My friends name is Zack" << endl ;
+    cout << "His favorite movie is Despicable Me" << endl;
+    cout << "He enjoys cycling" << endl ;
+return 0;
+}
